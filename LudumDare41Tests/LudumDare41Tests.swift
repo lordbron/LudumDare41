d121fe93ffeb79a83aece95b441182a6bef9132f
Tom Ortega <tom.ortega@gmail.com>
2018-04-21 02:57:16 -0700

    Initial Commit

---
+++ LudumDare41Tests/LudumDare41Tests.swift
@@ -0,0 +1,36 @@
+//
+//  LudumDare41Tests.swift
+//  LudumDare41Tests
+//
+//  Created by Thomas Ortega II on 4/21/18.
+//  Copyright © 2018 OO. All rights reserved.
+//
+
+import XCTest
+@testable import LudumDare41
+
+class LudumDare41Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
